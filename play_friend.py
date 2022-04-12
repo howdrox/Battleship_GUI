55aa62056d96b964f81b4e4f3da2012ed32ae5bb
WyzzR <louiskusno@gmail.com>
2022-04-12 17:53:42 +0200

    update file

--- play_friend.py
+++ play_friend.py
@@ -1,3 +1,19 @@
 """play_1v1.py: Multiplayer battleship game 1v1"""
 
-print("Hello world")
+import pygame
+
+size = width, height = (1700, 690)
+
+screen = pygame.display.set_mode(size)
+background = pygame.image.load("./im./background.png")
+
+
+run = True
+while run:
+
+    # event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
