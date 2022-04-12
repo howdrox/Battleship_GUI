33214c6aefc65804e08c97e3632da5f253cbc70b
Shaunerie <103446961+Shaunerie@users.noreply.github.com>
2022-04-12 18:14:18 +0200

    Add files via upload

---
+++ main_menu.py
@@ -0,0 +1,51 @@
+import pygame
+
+def main():
+    # Initialisation
+    pygame.init()
+    
+    # Afficher le logo
+    logo = pygame.image.load("img\\logo.jpg")
+    pygame.display.set_icon(logo)
+
+    # Titre de la fenêtre
+    pygame.display.set_caption("La bataille navale super cool de KUSNO & WOJTUSCISZYN & SAMAIN.")
+     
+    # Création de l'écran
+    screen = pygame.display.set_mode((1700,720))
+
+    #Images
+    background = pygame.image.load("img\\background.jpg")
+    cursor = pygame.image.load("img\\cursor.png")
+    cursor = pygame.transform.scale(cursor, (20, 20)) 
+
+    #Affichage d'images
+    screen.blit(background, (0,0))
+
+    #Condition
+    running = True
+
+    #Ne pas afficher le curseur
+    pygame.mouse.set_visible(False)
+
+    #Boucle principale
+    while running:
+        #Actualisation continue de l'écran:
+        pygame.display.flip()
+
+        #On prend tout ce qui se passe sur l'écran
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                screen.blit(background, (0,0))
+                screen.blit(cursor, event.pos)
+
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__ == "__main__":
+    # call the main function
+    main()
